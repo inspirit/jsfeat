722211f89183a069fd11a8b8413a0cc23ce2f7f4
Eugene Zatepyakin <zatepyakin@gmail.com>
2012-11-08 17:02:35 +0400

    initial commit

---
+++ src/jsfeat.js
@@ -0,0 +1,12 @@
+/**
+ * @author Eugene Zatepyakin / http://inspirit.ru/
+ */
+
+// namespace ?
+var jsfeat = jsfeat || { REVISION: 'ALPHA' };
+
+// use typed arrays if possible
+self.Int32Array = self.Int32Array || Array;
+self.Uint32Array = self.Uint32Array || Array;
+self.Uint8Array = self.Uint8Array || Array;
+self.Float32Array = self.Float32Array || Array;
