98d1fe760df500d8a4115c9a649ffe5ed851c030
Eugene Zatepyakin <zatepyakin@gmail.com>
2012-11-08 18:46:33 +0400

    roll back to original Math class

--- src/jsfeat_math.js
+++ src/jsfeat_math.js
@@ -9,20 +9,9 @@
     var math = (function() {
 
         return {
-            imod3: function(a) {
-                a = ((a >> 16) + (a & 0xFFFF)); /* sum base 2**16 digits a <= 0x1FFFE */
-                a = ((a >>  8) + (a & 0xFF));   /* sum base 2**8 digits a <= 0x2FD */
-                a = ((a >>  4) + (a & 0xF));    /* sum base 2**4 digits a <= 0x3C; worst case 0x3B */
-                a = ((a >>  4) + (a & 0xF));    /* sum base 2**4 digits a <= 0x3C; worst case 0x3B */
-                a = ((a >>  2) + (a & 0x3));    /* sum base 2**2 digits a <= 0x1D; worst case 0x1B */
-                a = ((a >>  2) + (a & 0x3));    /* sum base 2**2 digits a <= 0x9; worst case 0x7 */
-                //a = ((a >>  2) + (a & 0x3));    /* sum base 2**2 digits a <= 0x4 */
-                //if (a > 2) a = (a - 3);
-                return (a - 3*(a > 2));
-            },
-            fabs: function (value) {
-                return value * (1.0 - ((value < 0.0) << 1));
-            }
+            // looks like latest V8 engine is super smart :)
+            // so it turns none of hacks i published here can beat Math class
+            // probably next time...
         };
 
     })();
