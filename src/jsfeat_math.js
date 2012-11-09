9b41bd177806bf80c5a7d33589ab26061c030dce
Eugene Zatepyakin <zatepyakin@gmail.com>
2012-11-09 18:58:44 +0400

    additions & improvements
    gaussian filter
    generic qsort implementation
    median for numeric vectors
    imgproc methods optimized with loop unrolling
    added simple build scripts to concat and minify lib

--- src/jsfeat_math.js
+++ src/jsfeat_math.js
@@ -9,9 +9,274 @@
     var math = (function() {
 
         return {
-            // looks like latest V8 engine is super smart :)
-            // so it turns none of hacks i published here can beat Math class
-            // probably next time...
+            get_gaussian_kernel: function(size, sigma, kernel, data_type) {
+                var i=0,x=0.0,t=0.0,sigma_x=0.0,scale_2x=0.0;
+                var sum = 0.0;
+                var _kernel = new Float32Array(size);
+
+                if((size&1) == 1 && size <= 7 && sigma <= 0) {
+                    switch(size>>1) {
+                        case 0:
+                        _kernel[0] = 1.0;
+                        sum = 1.0;
+                        break;
+                        case 1:
+                        _kernel[0] = 0.25, _kernel[1] = 0.5, _kernel[2] = 0.25;
+                        sum = 0.25+0.5+0.25;
+                        break;
+                        case 2:
+                        _kernel[0] = 0.0625, _kernel[1] = 0.25, _kernel[2] = 0.375, 
+                        _kernel[3] = 0.25, _kernel[4] = 0.0625;
+                        sum = 0.0625+0.25+0.375+0.25+0.0625;
+                        break;
+                        case 3:
+                        _kernel[0] = 0.03125, _kernel[1] = 0.109375, _kernel[2] = 0.21875, 
+                        _kernel[3] = 0.28125, _kernel[4] = 0.21875, _kernel[5] = 0.109375, _kernel[6] = 0.03125;
+                        sum = 0.03125+0.109375+0.21875+0.28125+0.21875+0.109375+0.03125;
+                        break;
+                    }
+                } else {
+                    sigma_x = sigma > 0 ? sigma : ((size-1)*0.5 - 1.0)*0.3 + 0.8;
+                    scale_2x = -0.5/(sigma_x*sigma_x);
+
+                    for( ; i < size; ++i )
+                    {
+                        x = i - (size-1)*0.5;
+                        t = Math.exp(scale_2x*x*x);
+
+                        _kernel[i] = t;
+                        sum += t;
+                    }
+                }
+
+                if(data_type & jsfeat.U8_t) {
+                    // int based kernel
+                    sum = 256.0/sum;
+                    for (i = 0; i < size; ++i) {
+                        kernel[i] = (_kernel[i] * sum + 0.5)|0;
+                    }
+                } else {
+                    // classic kernel
+                    sum = 1.0/sum;
+                    for (i = 0; i < size; ++i) {
+                        kernel[i] = _kernel[i] * sum;
+                    }
+                }
+            },
+
+            // The current implementation was derived from *BSD system qsort():
+            // Copyright (c) 1992, 1993
+            // The Regents of the University of California.  All rights reserved.
+            qsort: function(array, low, high, cmp) {
+                var isort_thresh = 7;
+                var t;
+                var sp = 0,left=0,right=0,i=0,n=0,m=0,ptr=0,ptr2=0,d=0;
+                var left0=0,left1=0,right0=0,right1=0,pivot=0,a=0,b=0,c=0,swap_cnt=0;
+
+                var stack = new Int32Array(48*2);
+
+                if( (high-low+1) <= 1 ) return;
+
+                stack[0] = low;
+                stack[1] = high;
+
+                while( sp >= 0 ) {
+                
+                    left = stack[sp<<1];
+                    right = stack[(sp<<1)+1];
+                    sp--;
+
+                    for(;;) {
+                        n = (right - left) + 1;
+
+                        if( n <= isort_thresh ) {
+                        //insert_sort:
+                            for( ptr = left + 1; ptr <= right; ptr++ ) {
+                                for( ptr2 = ptr; ptr2 > left && cmp(array[ptr2],array[ptr2-1]); ptr2--) {
+                                    t = array[ptr2];
+                                    array[ptr2] = array[ptr2-1];
+                                    array[ptr2-1] = t;
+                                }
+                            }
+                            break;
+                        } else {
+                            swap_cnt = 0;
+
+                            left0 = left;
+                            right0 = right;
+                            pivot = left + (n>>1);
+
+                            if( n > 40 ) {
+                                d = n >> 3;
+                                a = left, b = left + d, c = left + 2*d;
+                                left = cmp(array[a], array[b]) ? (cmp(array[b], array[c]) ? b : (cmp(array[a], array[c]) ? c : a))
+                                                  : (cmp(array[c], array[b]) ? b : (cmp(array[a], array[c]) ? a : c));
+
+                                a = pivot - d, b = pivot, c = pivot + d;
+                                pivot = cmp(array[a], array[b]) ? (cmp(array[b], array[c]) ? b : (cmp(array[a], array[c]) ? c : a))
+                                                  : (cmp(array[c], array[b]) ? b : (cmp(array[a], array[c]) ? a : c));
+
+                                a = right - 2*d, b = right - d, c = right;
+                                right = cmp(array[a], array[b]) ? (cmp(array[b], array[c]) ? b : (cmp(array[a], array[c]) ? c : a))
+                                                  : (cmp(array[c], array[b]) ? b : (cmp(array[a], array[c]) ? a : c));
+                            }
+
+                            a = left, b = pivot, c = right;
+                            pivot = cmp(array[a], array[b]) ? (cmp(array[b], array[c]) ? b : (cmp(array[a], array[c]) ? c : a))   
+                                               : (cmp(array[c], array[b]) ? b : (cmp(array[a], array[c]) ? a : c));
+                            if( pivot != left0 ) {
+                                t = array[pivot];
+                                array[pivot] = array[left0];
+                                array[left0] = t;
+                                pivot = left0;
+                            }
+                            left = left1 = left0 + 1;
+                            right = right1 = right0;
+
+                            for(;;) {
+                            
+                                while( left <= right && !cmp(array[pivot], array[left]) ) {
+                                    if( !cmp(array[left], array[pivot]) ) {
+                                        if( left > left1 ) {
+                                            t = array[left1];
+                                            array[left1] = array[left];
+                                            array[left] = t;
+                                        }
+                                        swap_cnt = 1;
+                                        left1++;
+                                    }
+                                    left++;
+                                }
+
+                                while( left <= right && !cmp(array[right], array[pivot]) ) {
+                                    if( !cmp(array[pivot], array[right]) ) {
+                                        if( right < right1 ) {
+                                            t = array[right1];
+                                            array[right1] = array[right];
+                                            array[right] = t;
+                                        }
+                                        swap_cnt = 1;
+                                        right1--;
+                                    }
+                                    right--;
+                                }
+
+                                if( left > right ) break;
+                                
+                                t = array[left];
+                                array[left] = array[right];
+                                array[right] = t;
+                                swap_cnt = 1;
+                                left++;
+                                right--;
+                            }
+
+                            if( swap_cnt == 0 ) {
+                                left = left0, right = right0;
+                                //goto insert_sort;
+                                for( ptr = left + 1; ptr <= right; ptr++ ) {
+                                    for( ptr2 = ptr; ptr2 > left && cmp(array[ptr2],array[ptr2-1]); ptr2--) {
+                                        t = array[ptr2];
+                                        array[ptr2] = array[ptr2-1];
+                                        array[ptr2-1] = t;
+                                    }
+                                }
+                                break;
+                            }
+
+                            n = Math.min( (left1 - left0), (left - left1) );
+                            for( i = 0; i < n; i++ ) {
+                                t = array[left0+i];
+                                array[left0+i] = array[left+i-n];
+                                array[left+i-n] = t;
+                            }
+
+                            n = Math.min( (right0 - right1), (right1 - right) );
+                            for( i = 0; i < n; i++ ) {
+                                t = array[left+i];
+                                array[left+i] = array[right0+i-n+1];
+                                array[right0+i-n+1] = t;
+                            }
+                            n = (left - left1);
+                            m = (right1 - right);
+                            if( n > 1 ) {
+                                if( m > 1 ) {
+                                    if( n > m ) {
+                                        ++sp;
+                                        stack[sp<<1] = left0;
+                                        stack[(sp<<1)+1] = left0 + n - 1;
+                                        left = right0 - m + 1, right = right0;
+                                    } else {
+                                        ++sp;
+                                        stack[sp<<1] = right0 - m + 1;
+                                        stack[(sp<<1)+1] = right0;
+                                        left = left0, right = left0 + n - 1;
+                                    }
+                                } else {
+                                    left = left0, right = left0 + n - 1;
+                                }
+                            }
+                            else if( m > 1 )
+                                left = right0 - m + 1, right = right0;
+                            else
+                                break;
+                        }
+                    }
+                }
+            },
+
+            median: function(array, low, high) {
+                var w;
+                var middle=0,ll=0,hh=0,median=(low+high)>>1;
+                for (;;) {
+                    if (high <= low) return array[median];
+                    if (high == (low + 1)) {
+                        if (array[low] > array[high]) {
+                            w = array[low];
+                            array[low] = array[high];
+                            array[high] = w;
+                        }
+                        return array[median];
+                    }
+                    middle = ((low + high) >> 1);
+                    if (array[middle] > array[high]) {
+                        w = array[middle];
+                        array[middle] = array[high];
+                        array[high] = w;
+                    }
+                    if (array[low] > array[high]) {
+                        w = array[low];
+                        array[low] = array[high];
+                        array[high] = w;
+                    }
+                    if (array[middle] > array[low]) {
+                        w = array[middle];
+                        array[middle] = array[low];
+                        array[low] = w;
+                    }
+                    ll = (low + 1);
+                    w = array[middle];
+                    array[middle] = array[ll];
+                    array[ll] = w;
+                    hh = high;
+                    for (;;) {
+                        do ++ll; while (array[low] > array[ll]);
+                        do --hh; while (array[hh] > array[low]);
+                        if (hh < ll) break;
+                        w = array[ll];
+                        array[ll] = array[hh];
+                        array[hh] = w;
+                    }
+                    w = array[low];
+                    array[low] = array[hh];
+                    array[hh] = w;
+                    if (hh <= median)
+                        low = ll;
+                    else if (hh >= median)
+                        high = (hh - 1);
+                }
+                return 0;
+            }
         };
 
     })();
